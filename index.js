d10ab30411b8262f39aa5d804896b8f49de2bcac
Felipe Piubello <felipe.appmasters@gmail.com>
2019-12-11 15:53:04 +0100

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import {requireNativeComponent, ViewPropTypes} from 'react-native';
 import PropTypes from 'prop-types';
 
-module.exports = requireNativeComponent('MyCustomReactViewManager', null);
+module.exports = requireNativeComponent('sample_android_ui', null);
